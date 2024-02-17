7db86a0398b116cef79b45f0a95f9f6be9964dae
William Eze <eze.w@etoncollege.org.uk>
2024-02-17 18:20:29 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by William Eze on 17/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
