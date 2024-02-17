ae5c2ab3fd4345f87a39894c7ffaeff07109b1db
William Eze <eze.w@etoncollege.org.uk>
2024-02-17 18:32:29 +0000

    adding folder sturcture and basic model classes

---
+++ AbsenceRecorder/Models/Division.swift
@@ -0,0 +1,8 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by William Eze on 17/02/2024.
+//
+
+import Foundation
---
+++ AbsenceRecorder/Models/Student.swift
@@ -0,0 +1,8 @@
+//
+//  Student.swift
+//  AbsenceRecorder
+//
+//  Created by William Eze on 17/02/2024.
+//
+
+import Foundation
