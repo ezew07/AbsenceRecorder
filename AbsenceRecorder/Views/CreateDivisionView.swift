5cdd9adb678e81b67b088d9cd8e15107ffff3c69
William Eze <eze.w@etoncollege.org.uk>
2024-02-23 00:26:25 +0000

    Implemented an add division button conditional on sensible input

---
+++ AbsenceRecorder/Views/CreateDivisionView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateDivisionView.swift
+//  AbsenceRecorder
+//
+//  Created by William Eze on 22/02/2024.
+//
+
+import SwiftUI
+
+struct CreateDivisionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateDivisionView()
+}
